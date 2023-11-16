3e80f6740c813ff5d94200907dc671ac7ee04043
QuangMphann <quangphan2705@gmail.com>
2023-11-16 10:16:24 +0700

    added login and profile pages

--- src/components/elements/Header.tsx
+++ src/components/elements/Header.tsx
@@ -22,81 +22,36 @@ const Header = () => {
                         </a>
                     </div>
                     <div className="md:flex md:items-center md:gap-12">
-                        <nav aria-label="Global" className="hidden md:block">
-                            <ul className="flex items-center gap-6 text-sm">
-                                <li>
-                                    <a
-                                        className="text-gray-500 transition hover:text-gray-500/75"
-                                        href="/"
-                                    >
-                                        About
-                                    </a>
-                                </li>
-                                <li>
-                                    <a
-                                        className="text-gray-500 transition hover:text-gray-500/75"
-                                        href="/"
-                                    >
-                                        Careers
-                                    </a>
-                                </li>
-                                <li>
-                                    <a
-                                        className="text-gray-500 transition hover:text-gray-500/75"
-                                        href="/"
-                                    >
-                                        History
-                                    </a>
-                                </li>
-                                <li>
-                                    <a
-                                        className="text-gray-500 transition hover:text-gray-500/75"
-                                        href="/"
-                                    >
-                                        Services
-                                    </a>
-                                </li>
-                                <li>
-                                    <a
-                                        className="text-gray-500 transition hover:text-gray-500/75"
-                                        href="/"
-                                    >
-                                        Projects
-                                    </a>
-                                </li>
-                                <li>
-                                    <a
-                                        className="text-gray-500 transition hover:text-gray-500/75"
-                                        href="/"
-                                    >
-                                        Blog
-                                    </a>
-                                </li>
-                            </ul>
-                        </nav>
                         <div className="flex items-center gap-4">
-                            <div className="sm:flex sm:gap-4">
-                                <a
-                                    className="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow"
-                                    href="/login"
+                            <div className="sm:flex sm:gap-5 items-center">
+                                <Link
+                                    className="text-gray-500 transition hover:text-gray-500/75"
+                                    to="/"
                                 >
-                                    Login
-                                </a>
+                                    Home
+                                </Link>
+
+                                <Link
+                                    className="text-gray-500 transition hover:text-gray-500/75"
+                                    to="/profile"
+                                >
+                                    Profile
+                                </Link>
 
                                 <Link
                                     className="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow"
                                     to="/login"
                                 >
-                                    Link
+                                    Login
                                 </Link>
 
                                 <div className="hidden sm:flex">
-                                    <a
+                                    <Link
                                         className="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600"
-                                        href="/"
+                                        to="/login"
                                     >
                                         Register
-                                    </a>
+                                    </Link>
                                 </div>
                             </div>
                             <div className="block md:hidden">
--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,7 +1,88 @@
+import profilePicture from '../assets/a.jpg';
+import smallProfilePicture from '../assets/b.jpg';
+
 const ProfilePage = () => {
     return (
-        <div className="h-screen flex items-center justify-center">
-            Profilepage
+        <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+            <div className="container w-[1200px] m-auto flex justify-center items-center h-screen">
+                <div className="animate__animated animate__fadeIn animate__slower relative flex flex-col text-gray-700 bg-white shadow-md w-96 rounded-xl bg-clip-border">
+                    <div className="relative mx-4 mt-4 text-gray-700 bg-white shadow-lg rounded-xl bg-clip-border">
+                        <img
+                            src={profilePicture}
+                            className="w-full rounded-[10px]"
+                            alt="profile-picture"
+                        />
+                        <div className="rounded-full">
+                            <img
+                                className="custom-border"
+                                src={smallProfilePicture}
+                                alt="small-profile-picture"
+                            />
+                        </div>
+                    </div>
+                    <div className="p-4 pt-20 pb-0">
+                        <p className="block text-center font-sans text-3xl antialiased font-medium leading-relaxed text-transparent bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text">
+                            PHAN MINH QUANG
+                        </p>
+                        <p className="block pt-2 text-left font-sans text-2xl antialiased font-medium leading-relaxed bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text">
+                            WEB DEVELOPER
+                        </p>
+                        <div className="block font-sans leading-relaxed">
+                            <div>
+                                <span className="font-bold">
+                                    Date of birth:
+                                </span>
+                                27/05/2001
+                            </div>
+                            <div>
+                                <span className="font-bold">From:</span> Hanoi,
+                                Vietnam
+                            </div>
+                            <div>
+                                <span className="font-bold">Technologies:</span>
+                                HTML, CSS, SQL, PHP, Javascript, Tailwind,
+                                Bootstrap
+                            </div>
+                            <div>
+                                <span className="font-bold">Goal:</span> Aim to
+                                be an aspiring web developer who can create
+                                optimized, user-friendly web applications with
+                                beautiful visuals
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex justify-center p-2 pt-4 pb-4 gap-7">
+                        <a
+                            target="_blank"
+                            href="https://www.youtube.com/channel/UC5WFAjTQmwSVMj8ynLfukIg"
+                            className="flex items-center justify-center bg-white transform hover:-translate-y-3 border-2 w-12 h-12 rounded-full duration-500 text-red-500 border-red-500 hover:bg-red-500 hover:text-white text-2xl"
+                        >
+                            <i className="fab fa-youtube" />
+                        </a>
+                        <a
+                            target="_blank"
+                            href="https://www.facebook.com/QuangMphann"
+                            className="flex items-center justify-center bg-white duration-500 border-2 border-blue-600 w-12 transform hover:-translate-y-3 h-12 text-2xl rounded-full hover:bg-blue-600 hover:text-white text-blue-600"
+                        >
+                            <i className="fab fa-facebook" />
+                        </a>
+                        <a
+                            target="_blank"
+                            href="https://www.instagram.com/quangmphann/"
+                            className="flex items-center justify-center border-2 hover:border-0 border-pink-500 bg-white hover:bg-gradient-to-b text-2xl hover:from-indigo-600 hover:via-pink-600 hover:to-yellow-500 hover:text-white text-pink-600 w-12 h-12 transform hover:-translate-y-3 rounded-full duration-500"
+                        >
+                            <i className="fab fa-instagram" />
+                        </a>
+                        <a
+                            target="_blank"
+                            href="https://github.com/QuangMphan"
+                            className="flex items-center justify-center bg-white transform hover:-translate-y-3 border-2 w-12 h-12 rounded-full duration-500 text-black border-black hover:bg-black hover:text-white text-2xl"
+                        >
+                            <i className="fab fa-github" />
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
